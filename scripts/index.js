a48cf153f22bf90437556d3288cb406358691b7a
Igor Bocharov <bocharov.ia56@gmail.com>
2025-01-11 22:07:37 +0500

    Проект готов, отправка на ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,47 +1,30 @@
-// @todo: Темплейт карточки
-
-// @todo: DOM узлы
-
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
-
-
-// Start Script
-
 // Declaring variables
-
 const cardTemplate = document.querySelector('#card-template').content;
-console.log(cardTemplate)
-
 const cardList = document.querySelector('.places__list');
-console.log(cardList)
 
 // Function create card
-function createCard (element) {
+function createCard(element, removeCard) {
   const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
-  console.log(cardElement)
+  const deleteButton = cardElement.querySelector('.card__delete-button');
 
   cardElement.querySelector('.card__image').src = element.link;
   cardElement.querySelector('.card__image').alt = element.alt;
   cardElement.querySelector('.card__title').textContent = element.name;
 
+  deleteButton.addEventListener('click', () => removeCard(cardElement));
+
   return cardElement;
-};
+}
 
 // Function remove card
-function removeCard () {
-
-};
+function removeCard(item) {
+  item.remove();
+}
 
 // Function render one card
-function renderCard (cardElement, cardList) {
+function renderCard(cardElement, cardList) {
   cardList.append(cardElement);
-};
+}
 
 // Displaying cards using the forEach loop
-initialCards.forEach((element) => {
-  renderCard(createCard(element), cardList)
-});
+initialCards.forEach((element) => renderCard(createCard(element, removeCard), cardList));
