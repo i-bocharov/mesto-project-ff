92911b6f7c99c848cdeee69a7eb1e8a6489d9bbc
Igor Bocharov <bocharov.ia56@gmail.com>
2025-02-27 00:36:22 +0500

    Добавил проверку ссылок на аватарки

--- webpack.config.js
+++ webpack.config.js
@@ -24,12 +24,8 @@ module.exports = {
         use: 'babel-loader',
         exclude: '/node_modules/'
       },
-      // {
-      //   test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
-      //   type: 'asset/resource'
-      // },
       {
-        test: /\.(png|svg|jpg|gif)$/,
+        test: /\.(jpg|jpeg|png|gif|webp|svg)$/,
         type: 'asset/resource',
         generator: {
           filename: 'images/[name]_[hash][ext]'
